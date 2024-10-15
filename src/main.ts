dc564023bcd3f0b11440361a908e99163b0e6e2f
XiaozhiSans <xiaozhisans-awa@outlook.com>
2024-10-16 00:32:19 +0800

    ✨ feat: support dark scheme

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,14 @@ import LogoCanvas from './canvas';
 import loadSVGs from './utils/loadSVGs';
 import './i18n';
 
-(async function () {
-  await loadSVGs();
-  const logo = new LogoCanvas();
-  logo.draw();
+(async() => {
+	const matchScheme = matchMedia('(prefers-color-scheme: dark)');
+	matchScheme.addEventListener('change', (e) => {
+		document.documentElement.setAttribute('data-theme', e.matches? 'dark': 'light');
+	});
+	document.documentElement.setAttribute('data-theme', matchScheme.matches? 'dark': 'light');
+
+	await loadSVGs();
+	const logo = new LogoCanvas();
+	logo.draw();
 })();
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{jsx,js,ts,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('daisyui')],
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
-          primary: '#53bcfe',
-          secondary: '#2b2b2b',
-          'base-100': '#ffffff',
-          '--rounded-btn': '0.3rem',
-          '--rounded-box': '0.4rem',
-        },
-      },
-    ],
-  },
+	content: ['./index.html', './src/**/*.{jsx,js,ts,tsx}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [require('daisyui')],
+	daisyui: {
+		themes: [
+			{
+				light: {
+					primary: '#53bcfe',
+					secondary: '#2b2b2b',
+					'base-100': '#ffffff',
+					'--rounded-btn': '0.3rem',
+					'--rounded-box': '0.4rem',
+				},
+			},
+			{
+				dark: {
+					primary: '#53bcfe',
+					secondary: '#2b2b2b',
+					'base-100': '#16161a',
+					'--rounded-btn': '0.3rem',
+					'--rounded-box': '0.4rem',
+				}
+			}
+		],
+	},
 };
